217d31ade252161bf62c8e31d201c945873c4c2e
willianwt2000 <willianmetelien@gmail.com>
2023-12-19 20:42:34 -0500

    haciendo cambio

--- first-file/js/loader.js
+++ first-file/js/loader.js
@@ -1,11 +1,15 @@
-// JavaScript para ocultar el loader cuando la página ha cargado
-window.addEventListener('load', function () {
+// JavaScript para mostrar el loader mientras la página está cargando y ocultarlo cuando ha cargado completamente
+document.addEventListener('DOMContentLoaded', function () {
   var loader = document.getElementById('loader');
   var body = document.body;
 
-  // Oculta el loader
-  loader.style.display = 'none';
+  // Muestra el loader
+  loader.style.display = 'block';
 
-  // Muestra el contenido de la página
-  body.style.display = 'block';
+  // Oculta el loader después de que la página ha cargado completamente
+  window.addEventListener('load', function () {
+    loader.style.display = 'none';
+    // Muestra el contenido de la página
+    body.style.display = 'block';
+  });
 });
